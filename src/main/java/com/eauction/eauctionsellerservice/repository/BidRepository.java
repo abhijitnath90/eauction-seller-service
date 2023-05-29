85b655f3b691efd97eda2c01e5ec5360d37b5c19
unknown <nath.abhijit1990@gmail.com>
2023-05-29 20:30:58 +0530

    added db connection

--- src/main/java/com/eauction/eauctionsellerservice/repository/BidRepository.java
+++ src/main/java/com/eauction/eauctionsellerservice/repository/BidRepository.java
@@ -1,13 +1,14 @@
 package com.eauction.eauctionsellerservice.repository;
 
 import com.eauction.eauctionsellerservice.models.Bid;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BidRepository {
+public interface BidRepository extends MongoRepository<Bid, String> {
 
     List<Bid> findAllByProductId(String productId);
 
